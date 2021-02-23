9ea68f241a5aa6d563a8b080b199ecfda8a055a7
v-aivaras <aivaras.valasinas@gmail.com>
2021-02-23 22:13:47 +0000

    Show video preview

---
+++ includes/classes/PreviewProvider.php
@@ -0,0 +1,43 @@
+<?php
+class PreviewProvider {
+
+    private $con, $username;
+
+    public function __construct($con, $username) {
+        $this->con = $con;
+        $this->username = $username;
+    }
+
+    public function createPreviewVideo($entity) {
+        
+        if($entity == null) {
+            $entity = $this->getRandomEntity();
+        }
+        
+        $id = $entity->getId();
+        $name = $entity->getName();
+        $thumbnail = $entity->getThumbnail();
+        $preview = $entity->getPreview();
+
+        return "
+            <div class='previeContainer'>
+                <img src='$thumbnail' class='previewImage' hidden>
+
+                <video autoplay muted class='previewVideo'>
+                    <source src='$preview' type='video/mp4'>
+                </video>
+            </div>
+        ";
+    }
+
+    private function getRandomEntity() {
+
+        $query = $this->con->prepare("SELECT * FROM entities ORDER BY RAND() LIMIT 1");
+        $query->execute();
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+
+        return new Entity($this->con, $row);
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 require_once("includes/config.php");
+require_once("includes/classes/PreviewProvider.php");
+require_once("includes/classes/Entity.php");
 
 if(!isset($_SESSION["userLoggedIn"])) {
     header("Location: register.php");
 }
-echo $_SESSION["userLoggedIn"];
+
+$userLoggedIn = $_SESSION["userLoggedIn"];
+$preview = new PreviewProvider($con, $userLoggedIn);
+
+echo $preview->createPreviewVideo(null);
 ?>
