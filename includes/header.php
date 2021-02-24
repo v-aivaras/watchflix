709dd170ac8993e553580ba1a9dbd106cc5fbdd4
v-aivaras <aivaras.valasinas@gmail.com>
2021-02-24 21:27:49 +0000

    Working mute button, image after video ended

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,10 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
         <title>Welcome to Watchflix</title>
         <link rel="icon" href="assets/images/favicon.ico">
         <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="https://kit.fontawesome.com/17b288a8e6.js" crossorigin="anonymous"></script>
+        <script src="assets/js/script.js"></script>
+        
     </head>
     <body>
         <div class='wrapper'>
