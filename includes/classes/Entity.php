45110f50745b82963d9120e7cc5a6eccaa77e9c7
v-aivaras <aivaras.valasinas@gmail.com>
2021-02-27 12:32:41 +0000

    Display available seasons and videos

--- includes/classes/Entity.php
+++ includes/classes/Entity.php
@@ -9,7 +9,7 @@ class Entity {
         if(is_array($input)) {
             $this->sqlData = $input;
         } else {
-            $query = $this->con->prepare("SELECT * FROM entites WHERE id=:id");
+            $query = $this->con->prepare("SELECT * FROM entities WHERE id=:id");
             $query->bindValue(":id", $input);
             $query->execute();
 
@@ -31,6 +31,32 @@ class Entity {
 
     public function getPreview() {
         return $this->sqlData["preview"];
+    }
+
+    public function getSeasons() {
+        $query = $this->con->prepare("SELECT * FROM videos WHERE entityId=:id AND isMovie=0 ORDER BY season, episode ASC");
+        $query->bindValue(":id", $this->getId());
+        $query->execute();
+
+        $seasons = array();
+        $videos = array();
+        $currentSeason = null;
+        while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            
+            if($currentSeason != null && $currentSeason != $row["season"]) {
+                $seasons[] = new Season($currentSeason, $videos);
+                $videos = array();
+            }
+
+            $currentSeason = $row["season"];
+            $videos[] = new Video($this->con, $row);
+        }
+
+        if(sizeof($videos) != 0) {
+            $seasons[] = new Season($currentSeason, $videos);
+        }
+
+        return $seasons;
     }
 }
 
