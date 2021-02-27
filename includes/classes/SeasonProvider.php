c2910972d7ce221867b8eb9f2c298e1917a63e0d
v-aivaras <aivaras.valasinas@gmail.com>
2021-02-27 12:43:55 +0000

    Show suggested videos

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -48,7 +48,7 @@ class SeasonProvider {
                         <div class='contents'>
                             <img src='$thumbnail'>
                             <div class='videoInfo'>
-                                <h4>$name</h4>
+                                <h4> $episodeNumber. $name</h4>
                                 <span>$description</span>
                             </div>
                         </div>
