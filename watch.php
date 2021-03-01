d837d9e845167e66e7800b87d79210594abd3f0d
v-aivaras <aivaras.valasinas@gmail.com>
2021-03-01 18:58:43 +0000

    Show options when video ends

--- watch.php
+++ watch.php
@@ -8,6 +8,8 @@ if(!isset($_GET["id"])) {
 $video = new Video($con, $_GET["id"]);
 $video->incrementViews();
 
+$upNextVideo = VideoProvider::getUpNext($con, $video);
+
 ?>
 
 <div class="watchContainer">
@@ -20,7 +22,22 @@ $video->incrementViews();
 
     </div>
 
-    <video controls autoplay>
+    <div class="videoControls upNext" style="display:none;">
+        <button onclick="restartVideo();">
+            <i class="fas fa-redo"></i>
+        </button>
+
+        <div class="upNextContainer">
+            <h2>Up next:</h2>
+            <h3><?php echo $upNextVideo->getTitle(); ?></h3>
+            <h3><?php echo $upNextVideo->getSeasonAndEpisode(); ?></h3>
+            <button class="playNext" onclick="watchVideo(<?php echo $upNextVideo->getId(); ?>)">
+                <i class="fas fa-play"></i> Play
+            </button>
+        </div>
+    </div>
+
+    <video controls autoplay onended="showUpNext();">
         <source src='<?php echo $video->getFilePath(); ?>' type="video/mp4">
     </video>
 </div>
