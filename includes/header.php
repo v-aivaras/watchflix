9747104562c56c3e2b7f7f00f0fea0a6547be260
v-aivaras <aivaras.valasinas@gmail.com>
2021-03-02 19:34:17 +0000

    Navbar style and jquery

--- includes/header.php
+++ includes/header.php
@@ -30,26 +30,8 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
     <body>
         <div class='wrapper'>
 
-        <div class="topBar">
-            <div class="logoContainer">
-                <a href="index.php">
-                    <img src="assets/images/logo.png" alt="Logo">
-                </a>
-            </div>
-
-            <ul class="navLinks">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="shows.php">TV Shows</a></li>
-                <li><a href="movies.php">Movies</a></li>
-            </ul>
-
-            <div class="rightItems">
-                <a href="search.php">
-                    <i class="fas fa-search"></i>
-                </a>
-                    <i class="fas fa-user"></i>
-                <a href="profile.php">
-                    
-                </a>
-            </div>
-        </div>
+<?php
+if(!isset($hideNav)) {
+    include_once("includes/navBar.php");
+}
+?>
